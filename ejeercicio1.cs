ff867c26cfb30d3ae854c9f91fb3d4f8aa04a9cd
emonge23 <mongeedgard@gmail.com>
2022-05-25 08:28:34 -0600

    segundo commit solicitud de datos

--- ejeercicio1.cs
+++ ejeercicio1.cs
@@ -2,3 +2,8 @@ using system.collections;
 float a, b;
 float resultado;
 
+Console.WriteLine("digite el primer dato");
+a=float.parse(console.readline());
+console.WriteLine("Digite el segundo dato a promediar");
+b=float.parse(Console.readline());
+
