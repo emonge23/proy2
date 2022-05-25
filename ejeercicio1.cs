c0b342f85f598ab1b0e36784a5fa1bcd86ee3874
emonge23 <mongeedgard@gmail.com>
2022-05-25 08:40:13 -0600

    tercer commit version final

--- ejeercicio1.cs
+++ ejeercicio1.cs
@@ -1,4 +1,5 @@
 using system.collections;
+{
 float a, b;
 float resultado;
 
@@ -6,4 +7,8 @@ Console.WriteLine("digite el primer dato");
 a=float.parse(console.readline());
 console.WriteLine("Digite el segundo dato a promediar");
 b=float.parse(Console.readline());
+resultado=(a+b)/2;
+console.WriteLine("el promedio del estudiante es {0}", {resultado:N2});
+console.ReadKey();
 
+]
